89042ddb588063e0e566da484ed6fc9144d5f781
= <=>
2018-06-24 19:15:01 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="bg">
+        <div class="left">
+            <div class="logo">
+                <div class="logoImg"></div>
+                我就是LOGO
+            </div>
+        </div>
+        <div class="mid">
+            <div class="searchItem">
+                <input type="text" size="30" id="searchItem" placeholder="商品名稱">
+                <div class="icon">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+        </div>
+        <div class="right"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.bg {
+    width: 100%;
+    background-color: #ffefe9;
+    padding: 10px 0;
+    display: flex;
+    .left {
+        width: 15%;
+        .logo {
+            width: 160px;
+            height: 60px;
+            margin-left: calc((100% - 160px) / 2 );
+            border-bottom: 1px solid gray;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .logoImg {
+                width: 50px;
+                height: 50px;
+                background-image: url('../assets/Logo.png');
+                margin-right: 16px;
+                background-repeat: no-repeat;
+                background-size: 50px;
+            }
+        }
+    }
+    .mid {
+        width: 60%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .searchItem {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+            input {
+                width: 400px;
+                height: 36px;
+                border-radius: 8px 0 0 8px;
+                font-size: 20px;
+                font-weight: bold;
+                padding-left: 15px;
+                padding-top: 1px;
+                color: #696969;
+                outline: none;
+                border: 1px solid #696969;
+            }
+            input::-webkit-input-placeholder {
+                color: #ddd;
+            }
+            label {
+                font-size: 22px;
+                font-weight: bold;
+                margin-right: 5%;
+            }
+            .icon {
+                width: 36px;
+                height: 36px;
+                border: 1px solid #696969;
+                border-radius: 0 8px 8px 0;
+                background-color: #fab6ad;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding-top: 1px;
+                cursor: pointer;
+                i {
+                    font-size: 20px;
+                    color: #96645f;
+                }
+            }
+        }
+    }
+    .right {
+        width: 25%;
+    }
+}
+</style>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="bg">
+        <div class="left">
+            123
+        </div>
+        <div class="mid">
+            <div class="hotItem">
+                <div class="title">熱門商品</div>
+                <div class="items">
+                    <ul>
+                        <li v-for="i in 6">
+                            <div class="itemTitle">
+                                任天堂3DS
+                            </div>
+                            <div class="itemImg">
+
+                            </div>
+                            <div class="price">
+                               <i class="fas fa-money-bill-wave-alt"></i>
+                               <div class="priceCount">
+                                   3000
+                                </div>
+                               <div class="yuan">
+                                   元
+                               </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            789
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.bg {
+    width: 100%;
+    display: flex;    
+    .left {
+        width: 20%;
+    }
+    .mid {
+        width: 60%;
+        .hotItem {
+            padding: 20px;
+            .title {
+                width: 100%;
+                font-size: 28px;
+                font-weight: bold;
+                margin-bottom: 2.4%;
+                margin-left: 8%;
+            }
+            .items {
+                width: 100%;
+                ul {
+                    width: 100%;
+                    list-style: none;
+                    display: flex;
+                    flex-wrap: wrap; /* 超過長度自動換行 */
+                    padding-bottom: 2.4%;
+                    li {
+                        width: 200px;
+                        height: 270px;
+                        border: 1px solid #696969;
+                        padding: 15px 20px;
+                        margin-right: 3.2%;
+                        margin-left: 6.8%;
+                        margin-top: 2.4%;
+                        .itemTitle {
+                            font-size: 20px;
+                            width: 100%;
+                            text-align: center;
+                            margin-top: 4%;
+                        }
+                        .itemImg {
+                            width: 200px;
+                            height: 200px;
+                            background-image: url('../assets/3ds.jpg');
+                            background-size: 200px;
+                            background-repeat: no-repeat;
+                            background-position-y: 30%;
+                        }
+                        .price {
+                            font-size: 20px;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            i {
+                                color: #60b454;
+                                margin-right: 4%;
+                            }
+                            .priceCount {
+                                color: #f24;
+                                margin-right: 4%;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    .right {
+        width: 20%;
+    }
+}
+</style>
